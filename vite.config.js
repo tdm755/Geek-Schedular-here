16b79d6f90a1bea788f54b6aaba50559525deea3
Arpit Tiwari <tdm75513@gmail.com>
2024-09-18 14:55:33 +0530

    .

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias:{
+      '@': '/src',
+
+    },
+  },
+  server:{
+    port: 9977,
+    host: '0.0.0.0',
+  }
+})
